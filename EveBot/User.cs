2bed01ab509873642aa33752fec5ee704c59b78d
Nastassia <ucs13@live.ru>
2018-01-16 16:20:07 +0300

    edit User

--- EveBot/User.cs
+++ EveBot/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public int id;
+        public int telegramId;
+        public string firstName;
+        public string lastName;
+        public string userName;
+        public string passwordWeb;
+        public string nameForBot;
     }
 }
